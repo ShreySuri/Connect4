0b71be799dfdde74c44214369c9700fe6c17d355
Shrey Suri <shreysuri5@gmail.com>
2022-07-03 17:25:51 -0500

    working

--- test_file_3.py
+++ test_file_3.py
@@ -2,30 +2,55 @@ win_list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 
 co_ord = column * 10 + row
 
+
+def add_to_list(a, b, c, d, e, f, g, h, i, j, k, l, m):
+    win_list[a] = win_list[a] + 1
+    win_list[b] = win_list[b] + 1
+    win_list[c] = win_list[c] + 1
+    win_list[d] = win_list[d] + 1
+    win_list[e] = win_list[e] + 1
+    win_list[f] = win_list[f] + 1
+    win_list[g] = win_list[g] + 1
+    win_list[h] = win_list[h] + 1
+    win_list[i] = win_list[i] + 1
+    win_list[j] = win_list[j] + 1
+    win_list[k] = win_list[k] + 1
+    win_list[l] = win_list[l] + 1
+    win_list[m] = win_list[m] + 1
+    
+
+
+
+
+
+
+
+
+
+
+
 if co_ord == 11:
-    
+    add_to_list(0, 24, 45, 68, 68, 68, 68, 68, 68, 68, 68, 68, 68)
 elif co_ord == 21:
-    win_list[0] = win_list[0] + 1
-    win_list[24] = win_list[24] + 1
-    win_list[45] = win_list[45] + 1
+    add_to_list(0, 1, 27, 46, 68, 68, 68, 68, 68, 68, 68, 68, 68)
 elif co_ord == 31:
-    
+    add_to_list(0, 1, 2, 30, 47, 68, 68, 68, 68, 68, 68, 68, 68)
 elif co_ord == 41:
-
+    add_to_list(0, 1, 2, 3, 33, 48, 56, 68, 68, 68, 68, 68, 68)
 elif co_ord == 51:
-
+    add_to_list(1, 2, 3, 36, 57, 68, 68, 68, 68, 68, 68, 68, 68)
 elif co_ord == 61:
-
+    add_to_list(2, 3, 39, 58, 68, 68, 68, 68, 68, 68, 68, 68, 68)
 elif co_ord == 71:
-
+    add_to_list(3, 42, 59, 68, 68, 68, 68, 68, 68, 68, 68,  68, 68)
 elif co_ord == 12:
-
+    add_to_list(4, 24, 25, 49, 68, 68, 68, 68, 68, 68, 68, 68, 68)
 elif co_ord == 22:
-
+    add_to_list(4, 5, 27, 28, 45, 50, 68, 68, 68, 68, 68, 68, 68)
 elif co_ord == 32:
-
+    add_to_list(4, 5, 6, 30, 31, 46, 51, 56, 68, 68, 68, 68, 68)
 elif co_ord == 42:
-
+    
 elif co_ord == 52:
 
 elif co_ord == 62:
